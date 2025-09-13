a8492b87da5ce30e68556b9fa7d796242a6fcbc6
Брижашева Яна Михайловна <brizhashevaim@111c7.occ1.local>
2025-09-13 11:28:31 +0400

    First commit

---
+++ .bash_history
@@ -0,0 +1,17 @@
+php --version
+composter --version
+php --version
+composer --version
+php composer.phar create-project --prefer-dist laravel/laravel
+composer global require laravel/installer
+laravel new example-app
+CD laravel
+cd laravel
+ls
+home
+cd home
+cd Home
+ls
+composer create-project
+composer create - project --prefer-dist laravel/laravel
+Desktop
---
+++ .config/xfce4/terminal/accels.scm
@@ -0,0 +1,6 @@
+;------------------------------------------------------------------------------
+; Modified Calculate Utilities 3.7.6.9
+; Processed template files:
+; /var/db/repos/calculate/profiles/templates/6_ac_desktop_profile/2-user/x11-terms/xfce4-terminal-0.6.0/accels.scm
+;------------------------------------------------------------------------------
+(gtk_accel_path "<Actions>/terminal-window/contents" "")
